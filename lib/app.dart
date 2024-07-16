97f041eb29fc63d263fee53ff20061b51deb60f7
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-07-16 16:17:44 -0700

    chore: implements tests api with login

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,43 @@
 import 'package:flutter/material.dart';
 
-import './core/flavor/flavor_config.dart';
+import 'core/flavor/flavor_config.dart';
+import 'core/helpers/result.dart';
+import 'core/routes/app_routes.dart';
+import 'features/auth/data/auth_repository.dart';
 
 void bootstrap(FlavorConfig config) {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
 
   @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  void initState() {
+    super.initState();
+    initialize();
+  }
+
+  Future<void> initialize() async {
+    final AuthRepository authRepository = AuthRepository();
+    final result = await authRepository.login(email: 'leosilva@gmail.com', password: '12345678');
+    switch(result) {
+      case Success(object: final user):
+        debugPrint('Success ${user.id}');
+      case Failure(error: final error):
+        debugPrint(error.name);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp.router(
+      routerConfig: router,
+    );
   }
 }
