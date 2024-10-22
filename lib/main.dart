8c0f2d7aad2f45508167ef8db78267832499a347
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-10-22 12:49:16 -0300

    refactor: remove app code

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,9 @@ import './core/flavor/flavor_config.dart';
 import 'app.dart';
 
 void main() {
-    bootstrap(
-      FlavorConfig(
-        appId: '',
-        apiKey: '',
-        baseUrl: '',
-        flavor: AppFlavor.prod)
-    );
+  bootstrap(FlavorConfig(
+      appId: 'rC6gTn3o6JFMjSAnmKWTm7YFnKRWeOpEEHHcRoER',
+      apiKey: 'M7zGDkCPUn1fAbbBMtgZIhxmeB1E1EIZNZV4sedd',
+      baseUrl: 'https://parseapi.back4app.com/functions',
+      flavor: AppFlavor.prod));
 }
