96208d65fddeb2151e547733998e310854eaf11e
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-11-01 13:18:11 -0300

    feat: add device preferences orientation

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'package:app_agendamento/core/theme/app_theme.dart';
 import 'package:app_agendamento/core/widgets/alert/alert_area.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'core/di/di.dart';
 import 'core/flavor/flavor_config.dart';
@@ -15,8 +16,8 @@ Future<void> bootstrap(FlavorConfig config) async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
   await configureDependencies(config);
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(DevicePreview(
     builder: (_) => const App(),
     enabled: config.flavor == AppFlavor.prod, // TODO: mudar para: dev
@@ -40,9 +41,11 @@ class _AppState extends State<App> {
         routerConfig: router,
         locale: DevicePreview.locale(context),
         theme: ThemeData.light().copyWith(
-          scaffoldBackgroundColor: Colors.white,
-          textButtonTheme: const TextButtonThemeData(),
-        ),
+            scaffoldBackgroundColor: Colors.white,
+            textButtonTheme: const TextButtonThemeData(),
+            buttonTheme: const ButtonThemeData(
+              buttonColor: Colors.transparent,
+            )),
         builder: (context, child) {
           final newChild = Stack(
             children: [
