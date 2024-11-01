1c740f69a9a5525b1df00b85d6e168fe04132d99
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-11-01 16:38:06 -0300

    feat: add base page

---
+++ lib/core/widgets/app_base_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+
+import '../theme/app_theme.dart';
+import 'app_icon_button.dart';
+
+class AppBasePage extends StatelessWidget {
+  final String title;
+  final Widget body;
+  const AppBasePage({
+    super.key,
+    required this.title,
+    required this.body,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final AppTheme theme = context.watch();
+    return Scaffold(
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            padding: EdgeInsets.only(
+              top: MediaQuery.paddingOf(context).top + 64,
+              bottom: MediaQuery.paddingOf(context).bottom,
+            ),
+            child: body,
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Card(
+              color: theme.bg,
+              elevation: 8,
+              margin: EdgeInsets.zero,
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(36),
+                ),
+              ),
+              child: SafeArea(
+                bottom: false,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 4),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      AppIconButton(
+                        iconPath: 'assets/icons/chevron_left.svg',
+                        onPressed: context.pop,
+                      ),
+                      Expanded(
+                        child: Text(
+                          title,
+                          textAlign: TextAlign.center,
+                          style: theme.body16Bold,
+                        ),
+                      ),
+                      const SizedBox(width: 48)
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/features/auth/pages/signup/signup_page.dart
+++ lib/features/auth/pages/signup/signup_page.dart
@@ -1,8 +1,5 @@
-import 'package:app_agendamento/core/theme/app_theme.dart';
-import 'package:app_agendamento/core/widgets/app_icon_button.dart';
+import 'package:app_agendamento/core/widgets/app_base_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
 
 class SignupPage extends StatefulWidget {
   const SignupPage({super.key});
@@ -14,50 +11,11 @@ class SignupPage extends StatefulWidget {
 class _SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
-    final AppTheme theme = context.watch();
-    return Scaffold(
-      body: Stack(
-        children: [
-          Align(
-            alignment: Alignment.topCenter,
-            child: Card(
-              color: theme.bg,
-              elevation: 8,
-              margin: EdgeInsets.zero,
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.vertical(
-                  bottom: Radius.circular(36),
-                ),
-              ),
-              child: SafeArea(
-                bottom: false,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 4),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      AppIconButton(
-                        iconPath: 'assets/icons/chevron_left.svg',
-                        onPressed: context.pop,
-                      ),
-                      const Expanded(
-                        child: Text(
-                          'Cadastre-se',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(width: 48)
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
+    return AppBasePage(
+      title: 'Cadastre-se',
+      body: Container(
+        color: Colors.greenAccent,
+        height: 2000,
       ),
     );
   }
